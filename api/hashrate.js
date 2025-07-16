2b18035a60d43b4029c1d21271cef28670c5ff34
krausi99 <ajessicamelo@gmail.com>
2025-07-16 20:44:25 -0300

    Update hashrate.js

--- api/hashrate.js
+++ api/hashrate.js
@@ -1,8 +1,8 @@
 export default async function handler(req, res) {
-  const { model = "s21" } = req.query;
-  const HI_API_KEY = process.env.HI_API_KEY;
+  const { model = "s21" } = req.query
+  const HI_API_KEY = process.env.HI_API_KEY
 
-  const url = `https://api.hashrateindex.com/v1/hashrateindex/rigs/${model}`;
+  const url = `https://api.hashrateindex.com/v1/hashrateindex/rigs/${model}`
 
   try {
     const hiRes = await fetch(url, {
@@ -10,12 +10,13 @@ export default async function handler(req, res) {
         Accept: "application/json",
         "X-Hi-Api-Key": HI_API_KEY,
       },
-    });
+    })
 
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.status(hiRes.status).json(await hiRes.json());
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    res.status(hiRes.status).json(await hiRes.json())
   } catch (e) {
-    console.error(e);
-    res.status(500).json({ error: "Proxy error" });
+    console.error(e)
+    res.status(500).json({ error: "Proxy error" })
   }
 }
+
